424450bfeafc5444802c271fbd4b0370f25bd63d
Michael-Perillo <michael@perillo.me>
2025-05-31 19:47:22 -0400

    Linting setup
    ESLint + Prettier formatting

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
+// eslint-disable-next-line @angular-eslint/component-class-suffix
 export class App {
   protected title = 'app';
 }
