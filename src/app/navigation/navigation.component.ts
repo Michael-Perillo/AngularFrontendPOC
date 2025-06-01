577673445a635c646ff2cf1455034836a82a4e60
Michael-Perillo <michael@perillo.me>
2025-06-01 11:36:36 -0400

    Fix nav menu & styling, cleanup

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -6,9 +6,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
@@ -22,26 +19,9 @@ import { map, shareReplay } from 'rxjs/operators';
     MatIconModule,
     MatListModule
   ],
-  templateUrl: 'navigation.component.html',
-  styleUrl: 'navigation.component.scss',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  closeDrawerOnMobile(): void {
-    this.breakpointObserver.observe(Breakpoints.Handset)
-      .pipe(map(result => result.matches))
-      .subscribe(isHandset => {
-        if (isHandset) {
-          // We need to get the drawer reference here, but for this simple demonstration,
-          // we'll rely on the UI closing naturally after navigation
-        }
-      });
-  }
+  // Simplified component with no responsive handling
 }
