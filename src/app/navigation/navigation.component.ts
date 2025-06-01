c1edde191880fe996e5f0f62c86b24f5c5a4ff5a
Michael-Perillo <michael@perillo.me>
2025-06-01 11:23:05 -0400

    Initial routing/navigation setup
    Broken styles but routing & nav work

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  templateUrl: 'navigation.component.html',
+  styleUrl: 'navigation.component.scss',
+})
+export class NavigationComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  closeDrawerOnMobile(): void {
+    this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(map(result => result.matches))
+      .subscribe(isHandset => {
+        if (isHandset) {
+          // We need to get the drawer reference here, but for this simple demonstration,
+          // we'll rely on the UI closing naturally after navigation
+        }
+      });
+  }
+}
